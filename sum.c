c214a019a9c79fe98818f26713ea3d9e28c00ab8
Nandy219 <30311815+Nandy219@users.noreply.github.com>
2017-08-11 17:04:33 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n, i = 1, Sum = 0;
+  scanf("%d", &Number);
+  while(i <= Number)
+  {
+     Sum = Sum + i;
+     i++;
+  }
+  
+  printf("Sum = %d", Sum);
+  return 0;
+}
